a60507aa63270c69706441e319de205a5334573b
khaled J Muhammad <khaledblings@gmail.com>
2025-11-15 10:39:07 +0100

    Update logo text to 'NERIAH - The Light of God'

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,4 +1,4 @@
-import { CiCamera } from "react-icons/ci";
+I'mimport { CiCamera } from "react-icons/ci";
 import { Link } from "react-router-dom";
 
 const Logo = ({ path }) => {
@@ -8,7 +8,7 @@ const Logo = ({ path }) => {
       className="flex items-center gap-3 text-text-foreground tracking-wide font-bold text-[1.4rem] sm:text-[1.8rem] lg:text-[2rem] capitalize leading-5 sm:leading-0"
     >
       <CiCamera className="text-muted-foreground text-[2rem] sm:text-[2.6rem]" />
-      The Light concepts
+      NERIAH - The Light of God
     </Link>
   );
 };
